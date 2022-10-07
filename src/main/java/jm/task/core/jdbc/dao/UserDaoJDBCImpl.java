5f531386ae3ce70763a3bc3574cce15bcee3f473
Smashbasher <pablito.suhanov@yandex.ru>
2022-10-07 22:04:05 +0300

    Добавлены исправления

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -8,48 +8,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    Connection connection = null;
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-        connection = Util.getConnection();
-        try (Statement stateCreation = connection.createStatement()) {
+        try (Connection connection = Util.getConnection();
+                Statement stateCreation = connection.createStatement()) {
             stateCreation.executeUpdate("CREATE TABLE IF NOT EXISTS users " +
                     "(id BIGSERIAL PRIMARY KEY, name VARCHAR(255), last_name VARCHAR(255), age INT)");
-            connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     public void dropUsersTable() {
-        connection = Util.getConnection();
-        try (Statement stateDrop = connection.createStatement()) {
+        try (Connection connection = Util.getConnection();
+             Statement stateDrop = connection.createStatement()) {
             stateDrop.executeUpdate("DROP TABLE IF EXISTS users");
-            connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        connection = Util.getConnection();
-        try (PreparedStatement stateSaving = connection.prepareStatement("INSERT INTO users (name, last_name, age) VALUES (?, ?, ?)")) {
+        try (Connection connection = Util.getConnection();
+             PreparedStatement stateSaving = connection.prepareStatement("INSERT INTO users (name, last_name, age) VALUES (?, ?, ?)")) {
+
             stateSaving.setString(1, name);
             stateSaving.setString(2, lastName);
             stateSaving.setByte(3, age);
             stateSaving.executeUpdate();
-            connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     public void removeUserById(long id) {
-        connection = Util.getConnection();
-        try (PreparedStatement sateRemovement = connection.prepareStatement("DELETE FROM users WHERE id = ?")) {
+        try (Connection connection = Util.getConnection();
+             PreparedStatement sateRemovement = connection.prepareStatement("DELETE FROM users WHERE id = ?")) {
             sateRemovement.setLong(1, id);
             sateRemovement.executeUpdate();
             connection.close();
@@ -59,16 +56,15 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public List<User> getAllUsers() {
-        connection = Util.getConnection();
         List<User> allUsers = new ArrayList<>();
-        try (ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM users")) {
+        try (Connection connection = Util.getConnection();
+             ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM users")) {
             while(resultSet.next()) {
                 User user = new User(resultSet.getString("name"),
                         resultSet.getString("last_name"), resultSet.getByte("age"));
                 user.setId(resultSet.getLong("id"));
                 allUsers.add(user);
             }
-            connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -77,10 +73,9 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void cleanUsersTable() {
-        connection = Util.getConnection();
-        try (Statement stateClean = connection.createStatement()) {
+        try (Connection connection = Util.getConnection();
+             Statement stateClean = connection.createStatement()) {
             stateClean.executeUpdate("TRUNCATE TABLE users");
-            connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
